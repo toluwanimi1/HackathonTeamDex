09dba7ffbe8eeb9b7ecb01ce0c31b87969794127
Tolu <toluwanimi2@gmail.com>
2024-07-11 16:36:46 -0400

    Adding the 3d model to the page

--- src/Components/threedMenu.js
+++ src/Components/threedMenu.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import {useRef} from "react";
+import {Canvas, useFrame} from "@react-three/fiber"
+import { PerspectiveCamera, PresentationControls, Environment, ContactShadows, Html, useFBX } from '@react-three/drei';
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+
+const coffee = () => {
+  const fbx = useFBX('/Assets/cupandsaucer.fbx')
+  return <primitive object={fbx} scale={0.2} rotation={[2.2, -0.3, 0]} />;
+};
 
 const threedMenu = () => {
   return (
