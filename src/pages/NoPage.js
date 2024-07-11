9df8f73327328609f8365481cb597b35b92891b2
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-07-11 15:22:32 -0600

    added 404 page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { FaRegSadTear } from "react-icons/fa";
 
 const NoPage = () => {
-  return <h1>404 not found</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-slate-100 text-black">
+      <FaRegSadTear className="text-8xl mb-8 animate-bounce" />
+      <h1 className="text-5xl font-bold">404</h1>
+      <p className="text-3xl font-bold">Page Not Found</p>
+      <a href="/" className="mt-8 px-4 py-2 bg-slate-500 text-white rounded hover:bg-slate-800 transition duration-300">Back Home</a>
+    </div>
+  );
 };
 
 export default NoPage;
